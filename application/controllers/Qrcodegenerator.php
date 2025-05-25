599fb7657fb7d47812287ce003ae817a74dc2701
rashidweb1 <rashid.makent@gmail.com>
2025-05-25 16:41:43 +0530

    .

--- application/controllers/Qrcodegenerator.php
+++ application/controllers/Qrcodegenerator.php
@@ -56,7 +56,7 @@ class Qrcodegenerator extends CI_Controller {
         $qrcode = (new QRCode($options))->render($qr_digit);
         
         
-        echo '<img src="'.$qrcode.'" alt="QR Code" width="150px" height="150px"><p style="margin-left: 16px;font-size: 12px; margin-top: 2px;"><b>Cert:'.$certificate.'</b></p></div>';
+        echo '<img src="'.$qrcode.'" alt="QR Code" width="150px" height="150px"><p style="top: -10px;margin-left: 16px;font-size: 12px; margin-top: 2px;"><b>Cert:'.$certificate.'</b></p></div>';
 	}
 	
 	
